154b988ba8a4ea2ead1798f8c8fd9f9636dc7735
ad3ller <a.deller@ucl.ac.uk>
2016-04-30 18:54:34 +0100

    tweak init

--- positronium/__init__.py
+++ positronium/__init__.py
@@ -1,6 +1,5 @@
 #! python
 from __future__ import print_function, division  # Require Python 2.6 or later
-import positronium.constants
-import positronium.Bohr
-import positronium.decay
-import positronium.ionisation
+from . import constants
+from . import Bohr
+from .Ps import Ps
